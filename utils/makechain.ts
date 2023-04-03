992d7983603c1a88a4b48553a4fe1613819ae1cc
machaoui <machaoui@leyton.com>
2023-04-03 11:53:56 +0000

    uk urls removed & reverted links back in answers

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -4,7 +4,7 @@ import { SupabaseVectorStore } from 'langchain/vectorstores';
 import { PromptTemplate } from 'langchain/prompts';
 
 const CONDENSE_PROMPT =
-  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a professional question.
+  PromptTemplate.fromTemplate(`You can accept greeting. Given the following conversation and a follow up question, rephrase the follow up question to be a professional question.
 
 Chat History:
 {chat_history}
@@ -16,6 +16,7 @@ const QA_PROMPT = PromptTemplate.fromTemplate(
   If the question is in another language you can translate all your answers to the same language, including your introduction.
 You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided, do not make up anything.
 The answer should be short at maximum of three lines unless you're asked to explain more about it.
+Choose the exact relevant hyperlinks that matches the context provided. Do NOT make up any hyperlink that is not listed below.
 If you can't find the answer in the context below, just say "Hmm, maybe I'm missing something. Can you give more context?" Don't try to make up an answer.
 If the question is not related to Leyton or the context provided, politely inform them that you are tuned to only answer questions that are related to Leyton.
 
@@ -26,17 +27,25 @@ Question: {question}
 Answer in Markdown:`,
 );
 
+const modelNames = {
+    'davinci': 'text-davinci-003',
+    'turbo': 'gpt-3.5-turbo',
+}
+
 export const makeChain = (
   vectorstore: SupabaseVectorStore,
   onTokenStream?: (token: string) => void,
 ) => {
   const questionGenerator = new LLMChain({
-    llm: new OpenAI({ temperature: 0.4 }),
+    llm: new OpenAI({
+        temperature: 0.4,
+        modelName: modelNames['davinci'],
+    }),
     prompt: CONDENSE_PROMPT,
   });
   const docChain = loadQAChain(
     new OpenAI({
-      modelName: 'gpt-3.5-turbo',
+      modelName: modelNames['davinci'],
       temperature: 0.4,
       streaming: Boolean(onTokenStream),
       callbackManager: {
@@ -45,6 +54,10 @@ export const makeChain = (
     }),
     { prompt: QA_PROMPT },
   );
+
+    // console.log('questionGenerator', questionGenerator)
+    // console.log('docChain', docChain)
+    // console.log('vectorstore', vectorstore)
 
   return new ChatVectorDBQAChain({
     vectorstore,
